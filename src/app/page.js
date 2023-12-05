2bac863b88a17a35289b9c61a320e2621d0aa474
alamnahid <nahidalam13765@gmail.com>
2023-12-05 20:01:37 +0600

    create basic card

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
-
+import LatestNews from "@/components/ui/LatestNews/LatestNews";
+// import SideBar from "@/components/ui/SideBar/SideBar";
+import { Grid } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to home page</h1>
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <LatestNews />
+        </Grid>
+        <Grid item xs={4}>
+          {/* <SideBar /> */}
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
